45a893c563a6b02f60dfc4e83322310604754459
andyehrenberg <andyehrenberg@gmail.com>
2023-02-06 23:22:02 -0500

    tpu setup

---
+++ tpu_setup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/andyehrenberg/tiny_t5x.git
+cd tiny_t5x
+python3 -m pip install -e ".[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+wandb login
