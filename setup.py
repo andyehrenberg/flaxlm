4ab3b2ee22587bf52a1ad59fa50d64fffcbf71f6
andyehrenberg <andyehrenberg@gmail.com>
2023-02-06 23:05:21 -0500

    version

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ import setuptools
 # To enable importing version.py directly, we add its path to sys.path.
 version_path = os.path.join(os.path.dirname(__file__), 'tiny_t5x')
 sys.path.append(version_path)
-
-from version import __version__  # pylint: disable=g-import-not-at-top
+from version import __version__ 
 
 # Get the long description from the README file.
 with open('README.md') as fp:
-  _LONG_DESCRIPTION = fp.read()
+    long_description = fp.read()
 
 _jax_version = '0.4.3'
 
@@ -21,7 +20,7 @@ setuptools.setup(
     name='tiny_t5x',
     version=__version__,
     description='Flexible parallelism for training transformers',
-    long_description=_LONG_DESCRIPTION,
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Andy Ehrenberg',
     author_email='andyehrenberg@gmail.com',
