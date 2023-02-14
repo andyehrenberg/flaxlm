d91ab0e0b5b7b64c0f5f8d856ba06e11747cc0b7
andyehrenberg <andyehrenberg@gmail.com>
2023-02-14 11:50:30 -0500

    param spec

--- flaxlm/src/trainer.py
+++ flaxlm/src/trainer.py
@@ -216,8 +216,8 @@ class Trainer:
 
         p_create_fn = pjit.pjit(
             create_fn,
-            in_axis_resources=nn.logical_to_mesh(self.param_spec),
-            out_axis_resources=self.mesh_train_state_spec,
+            in_shardings=(nn.logical_to_mesh(self.param_spec),),
+            out_shardings=self.mesh_train_state_spec,
         )
 
         with self.mesh:
